d46d245880a0b81166b5d4e44ae68492ae680ad4
hritik017sh <hritik017sharma@gmail.com>
2022-09-27 22:20:53 +0530

    literals in java

---
+++ literals.java
@@ -0,0 +1,23 @@
+public class literals {
+    public static void main(String[] args) {
+  byte age=15;
+  short age2=23;
+  int age3=56;
+  long age4=45454545454545454l;
+  float a=23.3f;
+  double b=45.55;
+  char c='a';
+  boolean d=false ;
+  String str="hritik";
+      System.out.println(age);
+      System.out.println(age2);
+      System.out.println(age3);
+      System.out.println(age4);
+      System.out.println(a);
+      System.out.println(b);
+      System.out.println(c);
+      System.out.println(d);
+      System.out.println(str);
+
+    }
+}
