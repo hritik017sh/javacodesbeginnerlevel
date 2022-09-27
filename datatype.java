05d7d99cae3bb2df37580a7b3e48205e082abb71
hritik017sh <hritik017sharma@gmail.com>
2022-09-27 20:17:37 +0530

    variabledefines

---
+++ datatype.java
@@ -0,0 +1,10 @@
+public class datatype {
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=12;
+        int c=13;
+        int sum=a+b+c;
+        System.out.println(sum);
+    }
+}
