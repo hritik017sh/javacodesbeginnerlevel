abe1e26777d5ba39416f63cdf2aed4e48bc93765
hritik017sh <hritik017sharma@gmail.com>
2022-09-27 18:53:46 +0530

    PascalConvention and camelCaseConvention

--- hritik.java
+++ hritik.java
@@ -1,5 +1,7 @@
 public class hritik {
     public static void main(String[] args) {
         System.out.println("Hritik sharma");
+        //in classes we use=PascalConvention=starting word uppercase and a sebsequent word is also uppescase
+        //in function we use=camelCaseConvention=starting word lowercase and a sebsequent word is also uppercase
     }
 }
