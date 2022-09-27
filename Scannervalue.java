dca9b255d9c44fea13268a583329cf09022226b3
hritik017sh <hritik017sharma@gmail.com>
2022-09-27 23:22:41 +0530

    taking new input

---
+++ Scannervalue.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+public class Scannervalue {
+    public static void main(String[] args) {
+Scanner sc= new Scanner (System.in);
+   //     System.out.println("Enter first number=");
+  //      int a= sc.nextInt();
+  //      System.out.println("Enter second number=");
+   //     int b=sc.nextInt();
+  //      int sum=a+b;
+   //     System.out.println("The sum is="+sum);
+ //       boolean d =sc.hasNextInt();
+ //       System.out.println(d);
+        String st=sc.nextLine();
+        System.out.println(st);
+
+
+
+
+    }
+}
