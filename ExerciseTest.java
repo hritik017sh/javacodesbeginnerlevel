2f48f93c6bd1d4c12221f071f7862ccc36aeeac1
hritik017sh <hritik017sharma@gmail.com>
2022-09-28 23:24:03 +0530

    Exercise

---
+++ ExerciseTest.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class ExerciseTest{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        // Write a program to sum three numbers in java?
+  //        int a=sc.nextInt();
+  //       int b=sc.nextInt();
+  //      int c=sc.nextInt();
+   //     int sum=a+b+c;
+   //     System.out.println("the sum is="+sum);
+
+        //write a program a calculate cgpa using marks of three subjects.
+// float a=23;
+// float b=50;
+// float c=70;
+// float d=(a+b+c)/30;
+   //     System.out.println(d);
+
+        //write a java program which asks the users to enter his name and greets them  with "hello" <name> "have a good day" text
+//   System.out.println("Enter your name=");
+//    String name=sc.nextLine();
+//        System.out.println(" Hello " +name+ " have a nice day ");
+
+        //write a java programs to convert kilometers to miles
+       // System.out.println("Enter the number=");
+       //         int a=sc.nextInt();
+      //  double b=a*0.6213711922;
+      //  System.out.println(b);
+
+        //write a program to detect whether a number entered by the user is integer or not
+
+  //  System.out.println("Enter the number=");
+  //  System.out.println(sc.hasNextInt());
+    }
+
+}
