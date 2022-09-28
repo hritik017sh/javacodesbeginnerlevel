2c475b56e9a1ef4114ad408601d38b33a04d8d11
hritik017sh <hritik017sharma@gmail.com>
2022-09-28 19:30:18 +0530

    new datatypes added

--- datatype.java
+++ datatype.java
@@ -2,9 +2,10 @@ public class datatype {
     public static void main(String[] args)
     {
         int a=10;
-        int b=12;
+        long b=123452456245422524l;
         int c=13;
-        int sum=a+b+c;
+        long sum=a+b+c;
         System.out.println(sum);
+
     }
 }
