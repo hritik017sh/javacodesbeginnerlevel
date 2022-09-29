0215f52fd9e6182b9cdfa90c728cff927b9eaaff
hritik017sh <hritik017sharma@gmail.com>
2022-09-29 23:21:00 +0530

    all operators uses

---
+++ Operators.java
@@ -0,0 +1,53 @@
+
+import java.util.Scanner;
+public class Operators{
+    public static void main(String[] args)
+    {
+ // ----------Airthmetic operators are + - * / % ++ --    ----------
+     int a=10;
+     int b=20;
+     double c;
+//     c=a+b;                          --Addition happened--
+//     System.out.println(c);
+//     c=a-b;                          --Subtraction happened--
+//     System.out.println(c);
+//    c=a*b;                           --Multiplication happened--
+//        System.out.println(c);
+//       c=a/b;                         --Divide happened--
+//     System.out.println(c);
+//      c=a%b;                          --Modulus happened--
+//  System.out.println(c);
+//   c=a++;                             --Increment happened--
+//   System.out.println(c);
+//  c=b--;                              --Decrement happened--
+//   System.out.println(c);
+
+//        ---------- Assignment Operators   -------------
+//        System.out.println(a);        --So the value 10 stores in variable a--
+//        a+=b;                         --So here the value is a=a+b;--
+//        System.out.println(a);
+
+//      ---------- Comparison Operators    --------------
+//        System.out.println(a==b);       --In this it will compare a is equal to b if it is not then is will print false otherwise true --
+//        System.out.println(a!=b);       --In this if a is not equal to b it will print true otherwise false--
+//        System.out.println(a>b);        --In this a is greater than b then it will print true otherwise false --
+//        System.out.println(a<b);        --In this a is greater than b then it will print false otherwise true --
+
+//        ---------- Logical Operators   --------------
+//        System.out.println(a>b && a==b);  --It will show true when both the condition are true otherwise false --
+//        System.out.println(a<b || a==b);    --It will show true when one condition is  right otherwise false--
+
+//        ------------ Bitwise operators ____________
+//        System.out.println(7&4);
+//   111
+//   100                                      --let assume one is true and 0 is false then if both condition is true then it is true but if one is false then output is false--
+//   ---
+//   100                                  --It will convert in binary and then change binary into digit form--
+
+//        System.out.println(7|4);
+//   111
+//   100
+//   ---
+//   111                                 --Here if one condition is true then other will also change to true
+        }
+}
