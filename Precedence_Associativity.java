7c59aa7d79341c3bdcc5b514a92eddf4ddacdf42
hritik017sh <hritik017sharma@gmail.com>
2022-10-01 11:56:11 +0530

    how operators works in case of precedence and assositivity

---
+++ Precedence_Associativity.java
@@ -0,0 +1,49 @@
+public class Precedence_Associativity {
+    public static void main(String[] args){
+
+//                        -------- Precedence and Associativity -------------
+      // int a=10*7-15/3;
+               /*
+               =10*7-15/3                So here * / has higher precedence than - so first we do multiply
+               =70-15/3                  then we do divide and then substract no BODMASS is using there
+               =70-5
+               =65
+                */
+ //       System.out.println(a);
+        //int b=56/2-20*2;
+        /*
+        =56/2-20*2                   So here we first divide because of assotivity we know that
+        =28-20*2                     * / has same precedence so here if divide come first then
+        =28-40                       So we goes left to right assostivity.
+        =-12
+         */
+ //       System.out.println(b);
+
+        //               -------- Quiz time --------
+//    Q1.    ---- x-y/2-----
+        int a=4,b=2,c=2;
+//       int a=(x-y)/2;
+
+//    Q2.    ---b ka square - 4ac/2;----
+/*        int x=b*b-(4*a*c)/(2*a);
+        System.out.println(x);
+
+        =4-32/(2*4)
+        =4-32/8
+        =4-4
+        =0
+*/
+//     ------u*u-v*v------
+/*        int x=a*a-b*b;
+        System.out.println(x);
+
+        =4*4-2*2
+        =16-4
+        =12
+         */
+
+//    -----
+
+
+    }
+}
