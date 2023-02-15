f52a4ce2f236a53e38b62f49aea6307bd7a64e33
hritik017sh <hritik017sharma@gmail.com>
2023-02-16 00:32:03 +0530

    how to use scanner in java

---
+++ new3Scaner.java
@@ -0,0 +1,33 @@
+import java.lang.*;
+import java.util.*;
+
+class new3Scaner{
+    public static void main(String args[])
+    {
+     Scanner sc=new Scanner(System.in);
+//     int a=sc.nextInt();
+//     int b=sc.nextInt();
+//     int z=a+b;
+
+//        float a=sc.nextFloat();
+//        float b=sc.nextFloat();
+//        float z=a+b;
+//        System.out.println("the float sum is ="+z);
+
+
+
+//        System.out.println("May I know your name Sir");
+//        String a=sc.nextLine();
+//        System.out.println("Hii "+a);
+
+        sc.useRadix(3);
+                int x=sc.nextInt();
+        System.out.println(x);
+
+
+
+
+
+
+    }
+}
