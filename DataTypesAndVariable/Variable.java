6fd4824f899027101571cdb9ef22800bb79951d8
hritik017sh <hritik017sharma@gmail.com>
2023-02-18 00:47:26 +0530

    How to use variable

---
+++ DataTypesAndVariable/Variable.java
@@ -0,0 +1,20 @@
+package DataTypesAndVariable;
+
+
+public class Variable {
+    public static void main(String args[])
+    {
+        byte b=127;
+        int i=2300;
+        float f=23.4f;
+        char c ='A';
+
+//        System.out.println(b);
+//        System.out.println(i);
+//        System.out.println(f);
+//        System.out.println(c);
+
+
+    }
+
+}
