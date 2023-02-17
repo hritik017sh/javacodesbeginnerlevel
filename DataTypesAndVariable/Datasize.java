f7a2d9fd0f0b746dfca0bf2ffa03f1527f5d335f
hritik017sh <hritik017sharma@gmail.com>
2023-02-17 17:23:14 +0530

    how to use scanner in java

---
+++ DataTypesAndVariable/Datasize.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+class DataSize {
+    public static void main(String args[])
+    {
+        System.out.println("Int max range ="+ Integer.MAX_VALUE);
+        System.out.println("Int min range="+ Integer.MIN_VALUE);
+        System.out.println("Byte value="+ Integer.BYTES);
+
+    }
+
+
+
+}
