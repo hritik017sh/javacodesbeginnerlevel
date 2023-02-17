8f4209b34c82f80193d93ebfe2993a8644699b60
hritik017sh <hritik017sharma@gmail.com>
2023-02-17 17:42:34 +0530

    new datasize added

--- DataTypesAndVariable/Datasize.java
+++ DataTypesAndVariable/Datasize.java
@@ -1,12 +1,20 @@
 
+
 import java.util.*;
 class DataSize {
     public static void main(String args[])
     {
-        System.out.println("Int max range ="+ Integer.MAX_VALUE);
-        System.out.println("Int min range="+ Integer.MIN_VALUE);
-        System.out.println("Byte value="+ Integer.BYTES);
+//        System.out.println("Int max range ="+ Integer.MAX_VALUE);
+//        System.out.println("Int min range="+ Integer.MIN_VALUE);
+//        System.out.println("Int value="+ Integer.BYTES);
 
+//        System.out.println("float max value="+ Float.MAX_VALUE);
+//        System.out.println("float min value="+ Float.MIN_VALUE);
+//        System.out.println("flaot value"+ Float.BYTES);
+
+        System.out.println("Bytes max value="+ Byte.MAX_VALUE);
+        System.out.println("Bytes min value="+ Byte.MIN_VALUE);
+        System.out.println("Bytes value="+ Byte.BYTES);
     }
 
 
