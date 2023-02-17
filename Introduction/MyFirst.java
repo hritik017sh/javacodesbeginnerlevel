5b498d2de3b43cfeb1479a480db1648668fdf7ef
hritik017sh <hritik017sharma@gmail.com>
2023-02-17 17:29:54 +0530

    basic concept of java

--- Introduction/MyFirst.java
+++ Introduction/MyFirst.java
@@ -1,7 +1,10 @@
 import java.lang.*;
+import java.util.Set;
+
 class Myfirst{
     public static void main(String args[])
     {
         System.out.println("Hello Hritik sharma");
+
     }
 }
--- Introduction/new3Scaner.java
+++ Introduction/new3Scaner.java
@@ -37,5 +37,7 @@ class new3Scaner{
 
 
 
+
+
     }
 }
