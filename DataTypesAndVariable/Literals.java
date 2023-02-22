0ff340336ed13edb348576022230536e3455d74f
hritik017sh <hritik017sharma@gmail.com>
2023-02-22 23:29:30 +0530

    How literal works

---
+++ DataTypesAndVariable/Literals.java
@@ -0,0 +1,28 @@
+package DataTypesAndVariable;
+
+public class Literals {
+    public static void main(String args[])
+    {
+        int i=10;           //integer literal;
+        byte b=13;         //int literal;
+        short s=29;       //int literal;
+        float f=20.5f;   //float literal;
+        double d=34.5d; //double literal;
+        float dl=345_456_434; //we can use _ this if we have large value;
+
+        //"-----------------------------------"//
+
+        byte b1=10;      // Decimal value of 10
+        byte b2=0b1010; // Binary value of 10
+        byte b3=012;   // Octal value of 10
+        byte b4=0XA;  // Hexadecimal value of 10
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+
+
+    }
+
+}
