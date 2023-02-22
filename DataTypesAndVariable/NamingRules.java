ef8d7c362536aac1ce071af0d9a289f580e627e9
hritik017sh <hritik017sharma@gmail.com>
2023-02-22 22:37:21 +0530

    NamingConventionInJava

---
+++ DataTypesAndVariable/NamingRules.java
@@ -0,0 +1,18 @@
+package DataTypesAndVariable;
+
+public class NamingRules {
+    public static void main(String args[]) {
+        int x;
+        int X;
+        int a1; //contain alphabet first rather than,we use _ or $ these special classes;
+        int String=10;
+        System.out.println(String);
+        float averageMarksOfStudents=11;
+        System.out.println(averageMarksOfStudents);//so here we use camel cases methods
+        int avarage_methods$sosystematic_dataIntegralOfDataControlPanelIntegrityCOntainer$Data;
+        //No limit on Length of name
+        // int=float Should not be a keyboard
+
+
+    }
+}
