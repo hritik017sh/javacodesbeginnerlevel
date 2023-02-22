be07d9f90a5d23a4d7a2c6a752d0cdef90251983
hritik017sh <hritik017sharma@gmail.com>
2023-02-23 00:58:26 +0530

    Integral data types

---
+++ DataTypesAndVariable/IntegralDataTypes.java
@@ -0,0 +1,12 @@
+package DataTypesAndVariable;
+
+public class IntegralDataTypes {
+    public static void main(String args[])
+    {
+        int j=-5;
+        int i=11;
+        System.out.println(Integer.toBinaryString(j));
+        System.out.println(Integer.toHexString(i));
+        System.out.println(Integer.toOctalString(i));
+    }
+}
