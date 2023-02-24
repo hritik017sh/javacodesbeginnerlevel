8a41668e3166917ac05002779414ed8b9d1013a8
hritik017sh <hritik017sharma@gmail.com>
2023-02-24 16:39:05 +0530

    how type casting work in java

--- DataTypesAndVariable/IntegralDataTypes.java
+++ DataTypesAndVariable/IntegralDataTypes.java
@@ -9,4 +9,33 @@ public class IntegralDataTypes {
         System.out.println(Integer.toHexString(i));
         System.out.println(Integer.toOctalString(i));
     }
+
+    public static class JavaTypeCasting {
+        public static void main(String args[])
+        {
+            // Widening Casting//
+            // Widening Casting is done automatically when passing a smaller size type to a longer size type//
+            int myint=12;
+            double mydouble=myint;
+            System.out.println(myint);
+            System.out.println(mydouble);
+
+            //Narrowing casting//
+            //Narrowing casting is done manually by placing the type in parentheses in front of the value//
+            System.out.println("-----------");
+            double mydble=34.44;
+            int myent=(int) mydble;
+            System.out.println(mydble);
+            System.out.println(myent);
+
+        //CHANGING INT VALUE TO CHAR VALUE MEANS 64 IS A IN INT BECAUSE OF ASCII CODE
+            System.out.println("-----------");
+            int  i=68;
+             char mychar=(char) i;
+             System.out.println(mychar);
+
+
+
+        }
+    }
 }
