40bf295b2c9de483e0658d7e1522793bf123a0a9
hritik017sh <hritik017sharma@gmail.com>
2023-02-28 22:51:19 +0530

    new valaue

---
+++ DataTypesAndVariable/HowToUseString.java
@@ -0,0 +1,24 @@
+package DataTypesAndVariable;
+
+public class HowToUseString {
+    public static void main(String args[])
+    {
+        //How to find string length while we have a string given
+        String str="Hritik sharma";
+          int value=str.length();
+           System.out.println(value);
+
+        //How to make String capital letter into small letters
+        String strin="Akash Kumar";
+        String sstring=strin.toLowerCase();
+        System.out.println(sstring);
+
+        //How to use string small letter into capital letters
+        String smallletters="hritik sharmA";
+        String ustring=smallletters.toUpperCase();
+        System.out.println(ustring);
+        System.out.println(ustring);
+
+
+    }
+}
