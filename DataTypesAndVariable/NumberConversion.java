9534c39a0610473d3193734201765d6bb33df587
hritik017sh <hritik017sharma@gmail.com>
2023-03-20 14:50:15 +0530

    NumberConversion

---
+++ DataTypesAndVariable/NumberConversion.java
@@ -0,0 +1,13 @@
+package DataTypesAndVariable;
+
+public class NumberConversion {
+    public static void main(String args[])
+    {
+        int a=-56;
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toHexString(a));
+        System.out.println(Integer.toOctalString(a));
+
+    }
+
+}
