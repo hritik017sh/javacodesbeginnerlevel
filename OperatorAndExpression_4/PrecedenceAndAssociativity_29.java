b6e3fc66177af91c6b5db7e75ce2cf73317240be
hritik017sh <hritik017sharma@gmail.com>
2023-03-23 23:45:04 +0530

    airthmetic operator

---
+++ OperatorAndExpression_4/PrecedenceAndAssociativity_29.java
@@ -0,0 +1,18 @@
+package OperatorAndExpression_4;
+import com.sun.jdi.IntegerType;
+
+import java.util.*;
+public class PrecedenceAndAssociativity_29
+{
+    public static void main(String args[])
+    {
+       // How to use airthmetic operators of +(sum)
+       short  a=45,b=90;
+       int c=a+b;
+       c=a+b;
+        System.out.println(c);
+
+
+
+    }
+}
