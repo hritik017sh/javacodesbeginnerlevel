aa4898885e272a0520a89bace699eacec67e4447
hritik017sh <hritik017sharma@gmail.com>
2023-03-27 23:35:01 +0530

    operator

--- OperatorAndExpression_4/PrecedenceAndAssociativity_29.java
+++ OperatorAndExpression_4/PrecedenceAndAssociativity_29.java
@@ -7,16 +7,17 @@ public class PrecedenceAndAssociativity_29
     public static void main(String args[])
     {
        // How to use airthmetic operators of +(sum)
-       short  a=45,b=90;
+        short  a=45,b=90;
         int c=a+b;
+        System.out.println(c);
+
         float f=78.8f,d=78.9f;
         float fl=f+d;
         System.out.println(fl);
 
-        System.out.println(c);
 
         float f5=78.8f,d5=78.9f;
-        float fa=f+d;
+        float fa=f*d;
         System.out.println(fa);
 
 
