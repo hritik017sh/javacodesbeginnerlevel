c0e3cf2518dc07a8505710a7f795fe2dbc817b49
hritik017sh <hritik017sharma@gmail.com>
2023-04-04 12:49:59 +0530

    find ith bit

--- OperatorAndExpression_4/Bitwise.java
+++ OperatorAndExpression_4/Bitwise.java
@@ -5,15 +5,27 @@ public class Bitwise {
     {
         Scanner sc=new Scanner(System.in);
 //    Find Even or Odd with the help of Bitwise operations
-        System.out.println("Enter the no to find even or odd=");
-        int a=sc.nextInt();
-        if((a&1)==0)
+//        System.out.println("Enter the no to find even or odd=");
+//        int a=sc.nextInt();
+//        if((a&1)==0)
+//        {
+//            System.out.println("The number is even="+a);
+//
+//        }
+//        else
+//            System.out.println("The number is odd="+a);
+
+
+
+//   Find the ith bit of any number
+        int b=0b1110;
+       int  i=0;
+        if((1<<i&b)==0)
         {
-            System.out.println("The number is even="+a);
+            System.out.println("the no is 0");
 
         }
         else
-            System.out.println("The number is odd="+a);
-
+            System.out.println("The no is 1");
     }
 }
