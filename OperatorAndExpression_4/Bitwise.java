fe3ea20aa58cc28cd727724720c139a4e94694cb
hritik017sh <hritik017sharma@gmail.com>
2023-04-26 19:18:53 +0530

    while loop

--- OperatorAndExpression_4/Bitwise.java
+++ OperatorAndExpression_4/Bitwise.java
@@ -109,33 +109,43 @@ public class Bitwise {
 //        System.out.println(result);
 
 //        ----Switch case----
-        int day=sc.nextInt();
-        switch(day)
-        {
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-            default:
-                System.out.println("You entered a wrong number. You have to enter  1 to 7 to find weeks");
-        }
+//        int day=sc.nextInt();
+//        switch(day)
+//        {
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            case 4:
+//                System.out.println("Thursday");
+//                break;
+//            case 5:
+//                System.out.println("Friday");
+//                break;
+//            case 6:
+//                System.out.println("Saturday");
+//                break;
+//            case 7:
+//                System.out.println("Sunday");
+//            default:
+//                System.out.println("You entered a wrong number. You have to enter  1 to 7 to find weeks");
+//        }
 
+
+
+//       ----While condition----
+        int i=1;
+        while(i<=5)
+        {
+
+            System.out.println(i);
+            i++;
+        }
 
 
 
