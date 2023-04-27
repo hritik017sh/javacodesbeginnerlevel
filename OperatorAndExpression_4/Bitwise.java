a90bb56d4a134e11a51ae6bea72ca593b5874447
hritik017sh <hritik017sharma@gmail.com>
2023-04-27 19:53:18 +0530

    bitwise

--- OperatorAndExpression_4/Bitwise.java
+++ OperatorAndExpression_4/Bitwise.java
@@ -139,18 +139,30 @@ public class Bitwise {
 
 
 //       ----While condition----
-        int i=1;
-        while(i<=5)
+//        int i=1;
+//        while(i<=5)
+//        {
+//
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        ----Do While loop---
+
+//        int i=1;
+//     do{
+//         System.out.println(i);
+//         i++;
+//     }
+//     while(i<=5);
+
+//
+//        ----For loop----
+
+        for (int i=1;i<=5;i++)
         {
-
             System.out.println(i);
-            i++;
         }
-
-
-
-
-
 
 
 
