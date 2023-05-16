8a7ce13bedb09e251a034e82716a122f7e91f8f7
hritik017sh <hritik017sharma@gmail.com>
2023-05-16 10:01:01 +0530

    pattern

--- OperatorAndExpression_4/pattern.java
+++ OperatorAndExpression_4/pattern.java
@@ -343,17 +343,28 @@ public class pattern {
 //        }
 
 
-       int n=6;
-       int num=0;
-      for(int row=n;row>=1;row--)
-      {
-          for(int col=n;col>=row;col--)
-          {
-              System.out.print(col +" ");
+//       int n=6;
+//       int num=0;
+//      for(int row=n;row>=1;row--)
+//      {
+//          for(int col=n;col>=row;col--)
+//          {
+//              System.out.print(col +" ");
+//
+//          }
+//          System.out.println("");
+//       }
 
-          }
-          System.out.println("");
-       }
+
+//        int n=6;
+//        for(int row=1;row<=n;row++)
+//        {
+//            for(int col=row;col>=1;col--)
+//            {
+//                System.out.print( col+" ");
+//            }
+//            System.out.println(" ");
+//        }
 
 
 
