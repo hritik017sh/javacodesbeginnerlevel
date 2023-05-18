219c3b41c429d5cf20e4e0931e110fd5982fb27c
hritik017sh <hritik017sharma@gmail.com>
2023-05-19 00:31:24 +0530

    recursio

--- OperatorAndExpression_4/function.java
+++ OperatorAndExpression_4/function.java
@@ -1,16 +1,39 @@
 package OperatorAndExpression_4;
+import java.util.*;
 
 public class function {
     public static void fullname(String fname)
     {
         System.out.println(fname+" Sharma");
     }
+    public static void sum()
+    {
+        Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+      int b=sc.nextInt();
+      int c=a+b;
+      System.out.println(c);
+    }
+
+    public static int recursion(int start,int end)
+    {
+        if(end>start) {
+            return end + recursion(start,end-1);
+        }
+        else
+            return end;
+    }
     public static void main(String[] args)
     {
-        fullname("Hritik");
-        fullname("Naveen");
-        fullname("Tanuja");
-        fullname("Sweta");
+//        fullname("Hritik");
+//        fullname("Naveen");
+//        fullname("Tanuja");
+//        fullname("Sweta");
+//        sum();
+        int mynum=recursion(5,10);
+        System.out.println(mynum);
+
     }
+
 
 }
