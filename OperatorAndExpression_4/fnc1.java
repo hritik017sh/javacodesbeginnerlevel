2a4d50e6a5f026e6fe30e07e7f79db533489e3dd
hritik017sh <hritik017sharma@gmail.com>
2023-05-19 11:45:22 +0530

    how to use class and objects

---
+++ OperatorAndExpression_4/fnc1.java
@@ -0,0 +1,8 @@
+package OperatorAndExpression_4;
+
+public class fnc1 {
+    int x = 10;
+    public static void main(String[] args) {
+
+    }
+}
---
+++ OperatorAndExpression_4/fnc2.java
@@ -0,0 +1,9 @@
+package OperatorAndExpression_4;
+
+public class fnc2 {
+    public static void main(String[] args)
+    {
+        fnc1 myobj1=new fnc1();
+        System.out.println(myobj1.x);
+    }
+}
