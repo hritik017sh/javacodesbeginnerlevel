92c01deaed2f5353a48d2466d99ddd3e659cff6e
hritik017sh <hritik017sharma@gmail.com>
2023-05-21 00:55:42 +0530

    how to use protected modifiere

--- OperatorAndExpression_4/fnc2.java
+++ OperatorAndExpression_4/fnc2.java
@@ -1,9 +1,24 @@
 package OperatorAndExpression_4;
 
 public class fnc2 {
-    public static void main(String[] args)
-    {
-        fnc1 myobj1=new fnc1();
-        System.out.println(myobj1.x);
-    }
-}
+
+            protected String fname="Hritik";
+            protected String lname="Sharma";
+            protected String email="Hritik017sharma@gmail.com";
+            protected int age =23;
+
+        }
+        class Hritik extends fnc2
+        {
+            private int graduationyear=2018;
+            public static void main(String[] args)
+            {
+               Hritik  myobj=new Hritik();
+                System.out.println("Name: "+myobj.fname+" "+myobj.lname);
+                System.out.println("Name: "+myobj.email);
+                System.out.println("Name: "+myobj.age);
+                System.out.println("Name: "+myobj.graduationyear);
+
+            }
+        }
+
