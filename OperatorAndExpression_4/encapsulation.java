c7318cd175d31a8f9fdfebf4b6cdda9674107825
hritik017sh <hritik017sharma@gmail.com>
2023-05-24 01:03:33 +0530

    encapsulation

---
+++ OperatorAndExpression_4/encapsulation.java
@@ -0,0 +1,33 @@
+package OperatorAndExpression_4;
+
+public class encapsulation {
+    private String name  ="Hritik sharma";
+    private int age=24;
+    public int  getage()
+    {
+       return age;
+    }
+    public void setage(int a)
+    {
+        age=a;
+    }
+    public String getname()
+    {
+      return name;
+    }
+    public void setname(String s)
+    {
+        name=s;
+    }
+
+}
+class encap
+{
+    public static void main(String args[])
+    {
+        encapsulation myobj=new encapsulation();
+        myobj.setage(45);
+        myobj.setname("Hritik sharma");
+        System.out.println(myobj.getage()+":"+myobj.getname());
+    }
+}
