562410285dede83be15c77bf469eabc878faa15f
hritik017sh <hritik017sharma@gmail.com>
2023-05-23 23:11:30 +0530

    abstract classes use

--- OperatorAndExpression_4/fnc2.java
+++ OperatorAndExpression_4/fnc2.java
@@ -1,24 +1,64 @@
 package OperatorAndExpression_4;
 
-public class fnc2 {
+//public class fnc2 {
+//
+//            protected String fname="Hritik";
+//            protected String lname="Sharma";
+//            protected String email="Hritik017sharma@gmail.com";
+//            protected int age =23;
+//
+//        }
+//        class Hritik extends fnc2
+//        {
+//            private int graduationyear=2018;
+//            public static void main(String[] args)
+//            {
+//               Hritik  myobj=new Hritik();
+//                System.out.println("Name: "+myobj.fname+" "+myobj.lname);
+//                System.out.println("Name: "+myobj.email);
+//                System.out.println("Name: "+myobj.age);
+//                System.out.println("Name: "+myobj.graduationyear);
+//
+//            }
+//        }
 
-            protected String fname="Hritik";
-            protected String lname="Sharma";
-            protected String email="Hritik017sharma@gmail.com";
-            protected int age =23;
+ abstract public class fnc2
+{
+  abstract   public void bakshi();
+   abstract  public void aman();
+   public void akash()
+   {
+       System.out.println("Driving mustang");
+   }
 
-        }
-        class Hritik extends fnc2
+
+}
+ abstract class cars extends fnc2
+{
+    public void bakshi()
+    {
+        System.out.println("Driving fortuner");
+    }
+}
+    class carrs extends cars
+    {
+        public void aman()
         {
-            private int graduationyear=2018;
-            public static void main(String[] args)
-            {
-               Hritik  myobj=new Hritik();
-                System.out.println("Name: "+myobj.fname+" "+myobj.lname);
-                System.out.println("Name: "+myobj.email);
-                System.out.println("Name: "+myobj.age);
-                System.out.println("Name: "+myobj.graduationyear);
-
-            }
+            System.out.println("Driving lamborgini");
         }
+    }
+
+    class cars3
+    {
+        public static void main(String args[])
+        {
+           fnc2 obj=new carrs();
+           obj.aman();
+           obj.bakshi();
+           obj.akash();
+        }
+    }
+
+
+
 
