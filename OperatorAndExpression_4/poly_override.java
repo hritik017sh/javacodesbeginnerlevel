7f167865eaed17622ab7cc5e439e50bf6cdea648
hritik017sh <hritik017sharma@gmail.com>
2023-05-27 22:47:37 +0530

    how to use override with the help of polymorphism

---
+++ OperatorAndExpression_4/poly_override.java
@@ -0,0 +1,29 @@
+package OperatorAndExpression_4;
+
+public class poly_override {
+    public void sweta()
+    {
+        System.out.println(" Hii sweta 1");
+
+    }
+}
+
+    class tanu extends poly_override
+    {
+        public void sweta()
+        {
+            System.out.println("Hiii tanu");
+        }
+
+    }
+
+
+    class naveen
+    {
+        public static void main(String args[])
+        {
+            poly_override name=new  tanu();
+            name.sweta();
+        }
+    }
+
