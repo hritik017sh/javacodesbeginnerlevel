b5855ea764d3756a26dfb37729ed8a935b825e23
hritik017sh <hritik017sharma@gmail.com>
2023-06-07 22:35:00 +0530

    polymorphism

--- OperatorAndExpression_4/poly_override.java
+++ OperatorAndExpression_4/poly_override.java
@@ -30,9 +30,7 @@ public class poly_override {
         {
             poly_override name=new  tanu();
             name.sweta();
-            name.shreya();
-            name.sweta();
-            name.sweta();
+
 
 
         }
